6427b6ce5702f347f560c352af3f091c98173bc1
zet-wu <sg271828@mail.ustc.edu.cn>
2026-04-05 22:59:27 +0800

    init文件导出类

---
+++ LyricPlus/__init__.py
@@ -0,0 +1,18 @@
+from LyricPlus.Align import AlignedLyricLine, LyricAlignmentResult, LyricAligner
+from LyricPlus.Lyric import LyricLineStamp, LyricTokenLine
+from LyricPlus.Transcribe import WhisperChunkResult, WhisperTrackResult, WhisperTranscriber
+from LyricPlus.Vocal import VocalAnalysisResult, VocalAnalyzer, VocalSegment
+
+__all__ = [
+    "AlignedLyricLine",
+    "LyricAlignmentResult",
+    "LyricAligner",
+    "LyricLineStamp",
+    "LyricTokenLine",
+    "WhisperChunkResult",
+    "WhisperTrackResult",
+    "WhisperTranscriber",
+    "VocalAnalysisResult",
+    "VocalAnalyzer",
+    "VocalSegment",
+]
